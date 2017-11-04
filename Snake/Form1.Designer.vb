8a04bd1d378051642122cfe1cdf18b7c5d4d4820
Jack Steel <JackSteel97@users.noreply.github.com>
2017-11-04 16:22:04 +0000

    upload

---
+++ Snake/Form1.Designer.vb
@@ -0,0 +1,309 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ParentForm
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.components = New System.ComponentModel.Container()
+		Me.Snake_0 = New System.Windows.Forms.Label()
+		Me.Snake_1 = New System.Windows.Forms.Label()
+		Me.Snake_2 = New System.Windows.Forms.Label()
+		Me.Mover = New System.Windows.Forms.Timer(Me.components)
+		Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+		Me.Food = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+		Me.Snake_3 = New System.Windows.Forms.Label()
+		Me.scorelbl = New System.Windows.Forms.Label()
+		Me.MenuPanel = New System.Windows.Forms.Panel()
+		Me.TitleSnake_0 = New System.Windows.Forms.Label()
+		Me.settingspanel = New System.Windows.Forms.Panel()
+		Me.savebtn = New System.Windows.Forms.Button()
+		Me.changecolourbtn = New System.Windows.Forms.Button()
+		Me.SettingsBtn = New System.Windows.Forms.Button()
+		Me.Highscorestxt = New System.Windows.Forms.Label()
+		Me.Highscoresbtn = New System.Windows.Forms.Button()
+		Me.Difficultyselector = New System.Windows.Forms.ComboBox()
+		Me.Startbtn = New System.Windows.Forms.Button()
+		Me.Title = New System.Windows.Forms.Label()
+		Me.ColourChanger = New System.Windows.Forms.Timer(Me.components)
+		Me.Fader = New System.Windows.Forms.Timer(Me.components)
+		Me.scoredisp = New System.Windows.Forms.Label()
+		Me.Snakecolour = New System.Windows.Forms.ColorDialog()
+		Me.TitleMover = New System.Windows.Forms.Timer(Me.components)
+		Me.MenuPanel.SuspendLayout()
+		Me.settingspanel.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'Snake_0
+		'
+		Me.Snake_0.BackColor = System.Drawing.Color.White
+		Me.Snake_0.Location = New System.Drawing.Point(250, 210)
+		Me.Snake_0.Name = "Snake_0"
+		Me.Snake_0.Size = New System.Drawing.Size(10, 10)
+		Me.Snake_0.TabIndex = 0
+		'
+		'Snake_1
+		'
+		Me.Snake_1.BackColor = System.Drawing.Color.White
+		Me.Snake_1.Location = New System.Drawing.Point(240, 210)
+		Me.Snake_1.Name = "Snake_1"
+		Me.Snake_1.Size = New System.Drawing.Size(10, 10)
+		Me.Snake_1.TabIndex = 1
+		'
+		'Snake_2
+		'
+		Me.Snake_2.BackColor = System.Drawing.Color.White
+		Me.Snake_2.Location = New System.Drawing.Point(230, 210)
+		Me.Snake_2.Name = "Snake_2"
+		Me.Snake_2.Size = New System.Drawing.Size(10, 10)
+		Me.Snake_2.TabIndex = 2
+		'
+		'Mover
+		'
+		'
+		'ShapeContainer1
+		'
+		Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+		Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+		Me.ShapeContainer1.Name = "ShapeContainer1"
+		Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.Food})
+		Me.ShapeContainer1.Size = New System.Drawing.Size(732, 513)
+		Me.ShapeContainer1.TabIndex = 3
+		Me.ShapeContainer1.TabStop = False
+		'
+		'Food
+		'
+		Me.Food.BackColor = System.Drawing.Color.White
+		Me.Food.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque
+		Me.Food.FillColor = System.Drawing.Color.White
+		Me.Food.Location = New System.Drawing.Point(470, 210)
+		Me.Food.Name = "Food"
+		Me.Food.Size = New System.Drawing.Size(10, 10)
+		'
+		'Snake_3
+		'
+		Me.Snake_3.BackColor = System.Drawing.Color.White
+		Me.Snake_3.Location = New System.Drawing.Point(220, 210)
+		Me.Snake_3.Name = "Snake_3"
+		Me.Snake_3.Size = New System.Drawing.Size(10, 10)
+		Me.Snake_3.TabIndex = 4
+		'
+		'scorelbl
+		'
+		Me.scorelbl.AutoSize = True
+		Me.scorelbl.ForeColor = System.Drawing.Color.White
+		Me.scorelbl.Location = New System.Drawing.Point(12, 9)
+		Me.scorelbl.Name = "scorelbl"
+		Me.scorelbl.Size = New System.Drawing.Size(47, 13)
+		Me.scorelbl.TabIndex = 5
+		Me.scorelbl.Text = "Score: 0"
+		'
+		'MenuPanel
+		'
+		Me.MenuPanel.BackColor = System.Drawing.Color.Black
+		Me.MenuPanel.Controls.Add(Me.TitleSnake_0)
+		Me.MenuPanel.Controls.Add(Me.settingspanel)
+		Me.MenuPanel.Controls.Add(Me.SettingsBtn)
+		Me.MenuPanel.Controls.Add(Me.Highscorestxt)
+		Me.MenuPanel.Controls.Add(Me.Highscoresbtn)
+		Me.MenuPanel.Controls.Add(Me.Difficultyselector)
+		Me.MenuPanel.Controls.Add(Me.Startbtn)
+		Me.MenuPanel.Controls.Add(Me.Title)
+		Me.MenuPanel.Location = New System.Drawing.Point(12, 9)
+		Me.MenuPanel.Name = "MenuPanel"
+		Me.MenuPanel.Size = New System.Drawing.Size(705, 476)
+		Me.MenuPanel.TabIndex = 6
+		'
+		'TitleSnake_0
+		'
+		Me.TitleSnake_0.BackColor = System.Drawing.Color.White
+		Me.TitleSnake_0.Location = New System.Drawing.Point(445, 102)
+		Me.TitleSnake_0.Name = "TitleSnake_0"
+		Me.TitleSnake_0.Size = New System.Drawing.Size(10, 10)
+		Me.TitleSnake_0.TabIndex = 7
+		'
+		'settingspanel
+		'
+		Me.settingspanel.Controls.Add(Me.savebtn)
+		Me.settingspanel.Controls.Add(Me.changecolourbtn)
+		Me.settingspanel.Location = New System.Drawing.Point(18, 3)
+		Me.settingspanel.Name = "settingspanel"
+		Me.settingspanel.Size = New System.Drawing.Size(673, 401)
+		Me.settingspanel.TabIndex = 6
+		Me.settingspanel.Visible = False
+		'
+		'savebtn
+		'
+		Me.savebtn.Location = New System.Drawing.Point(103, 177)
+		Me.savebtn.Name = "savebtn"
+		Me.savebtn.Size = New System.Drawing.Size(75, 23)
+		Me.savebtn.TabIndex = 1
+		Me.savebtn.Text = "save"
+		Me.savebtn.UseVisualStyleBackColor = True
+		'
+		'changecolourbtn
+		'
+		Me.changecolourbtn.Location = New System.Drawing.Point(93, 25)
+		Me.changecolourbtn.Name = "changecolourbtn"
+		Me.changecolourbtn.Size = New System.Drawing.Size(172, 23)
+		Me.changecolourbtn.TabIndex = 0
+		Me.changecolourbtn.Text = "Change Snake Colour"
+		Me.changecolourbtn.UseVisualStyleBackColor = True
+		'
+		'SettingsBtn
+		'
+		Me.SettingsBtn.Font = New System.Drawing.Font("OCR A Extended", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.SettingsBtn.Location = New System.Drawing.Point(289, 266)
+		Me.SettingsBtn.Name = "SettingsBtn"
+		Me.SettingsBtn.Size = New System.Drawing.Size(97, 46)
+		Me.SettingsBtn.TabIndex = 5
+		Me.SettingsBtn.Text = "Settings"
+		Me.SettingsBtn.UseVisualStyleBackColor = True
+		'
+		'Highscorestxt
+		'
+		Me.Highscorestxt.AutoSize = True
+		Me.Highscorestxt.BackColor = System.Drawing.Color.Transparent
+		Me.Highscorestxt.Font = New System.Drawing.Font("Microsoft Sans Serif", 21.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Highscorestxt.ForeColor = System.Drawing.Color.White
+		Me.Highscorestxt.Location = New System.Drawing.Point(550, 75)
+		Me.Highscorestxt.Name = "Highscorestxt"
+		Me.Highscorestxt.RightToLeft = System.Windows.Forms.RightToLeft.No
+		Me.Highscorestxt.Size = New System.Drawing.Size(170, 33)
+		Me.Highscorestxt.TabIndex = 4
+		Me.Highscorestxt.Text = "Highscores:"
+		Me.Highscorestxt.Visible = False
+		'
+		'Highscoresbtn
+		'
+		Me.Highscoresbtn.Font = New System.Drawing.Font("OCR A Extended", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Highscoresbtn.Location = New System.Drawing.Point(289, 202)
+		Me.Highscoresbtn.Name = "Highscoresbtn"
+		Me.Highscoresbtn.Size = New System.Drawing.Size(97, 46)
+		Me.Highscoresbtn.TabIndex = 3
+		Me.Highscoresbtn.Text = "Highscores"
+		Me.Highscoresbtn.UseVisualStyleBackColor = True
+		'
+		'Difficultyselector
+		'
+		Me.Difficultyselector.Font = New System.Drawing.Font("Calibri", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Difficultyselector.FormattingEnabled = True
+		Me.Difficultyselector.Items.AddRange(New Object() {"Easy", "Intermediate", "Hard", "Insane", "Impossible"})
+		Me.Difficultyselector.Location = New System.Drawing.Point(392, 162)
+		Me.Difficultyselector.Name = "Difficultyselector"
+		Me.Difficultyselector.Size = New System.Drawing.Size(105, 21)
+		Me.Difficultyselector.TabIndex = 2
+		Me.Difficultyselector.Text = "Select Difficulty"
+		'
+		'Startbtn
+		'
+		Me.Startbtn.Font = New System.Drawing.Font("OCR A Extended", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Startbtn.Location = New System.Drawing.Point(289, 138)
+		Me.Startbtn.Name = "Startbtn"
+		Me.Startbtn.Size = New System.Drawing.Size(97, 45)
+		Me.Startbtn.TabIndex = 1
+		Me.Startbtn.Text = "Start Game"
+		Me.Startbtn.UseVisualStyleBackColor = True
+		'
+		'Title
+		'
+		Me.Title.AutoSize = True
+		Me.Title.BackColor = System.Drawing.Color.Transparent
+		Me.Title.Font = New System.Drawing.Font("Microsoft Sans Serif", 48.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Title.ForeColor = System.Drawing.Color.White
+		Me.Title.Location = New System.Drawing.Point(223, 16)
+		Me.Title.Name = "Title"
+		Me.Title.RightToLeft = System.Windows.Forms.RightToLeft.No
+		Me.Title.Size = New System.Drawing.Size(255, 73)
+		Me.Title.TabIndex = 0
+		Me.Title.Text = "SNAKE"
+		'
+		'ColourChanger
+		'
+		Me.ColourChanger.Interval = 50
+		'
+		'Fader
+		'
+		Me.Fader.Interval = 1
+		'
+		'scoredisp
+		'
+		Me.scoredisp.AutoSize = True
+		Me.scoredisp.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.scoredisp.ForeColor = System.Drawing.Color.White
+		Me.scoredisp.Location = New System.Drawing.Point(120, 0)
+		Me.scoredisp.Name = "scoredisp"
+		Me.scoredisp.Size = New System.Drawing.Size(0, 16)
+		Me.scoredisp.TabIndex = 7
+		'
+		'TitleMover
+		'
+		Me.TitleMover.Enabled = True
+		'
+		'ParentForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.Color.Black
+		Me.ClientSize = New System.Drawing.Size(732, 513)
+		Me.Controls.Add(Me.scoredisp)
+		Me.Controls.Add(Me.MenuPanel)
+		Me.Controls.Add(Me.scorelbl)
+		Me.Controls.Add(Me.Snake_3)
+		Me.Controls.Add(Me.Snake_2)
+		Me.Controls.Add(Me.Snake_1)
+		Me.Controls.Add(Me.Snake_0)
+		Me.Controls.Add(Me.ShapeContainer1)
+		Me.KeyPreview = True
+		Me.Name = "ParentForm"
+		Me.Text = "Snake"
+		Me.MenuPanel.ResumeLayout(False)
+		Me.MenuPanel.PerformLayout()
+		Me.settingspanel.ResumeLayout(False)
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+    Friend WithEvents Snake_0 As System.Windows.Forms.Label
+    Friend WithEvents Snake_1 As System.Windows.Forms.Label
+    Friend WithEvents Snake_2 As System.Windows.Forms.Label
+    Friend WithEvents Mover As System.Windows.Forms.Timer
+    Friend WithEvents Snake_3 As System.Windows.Forms.Label
+    Friend WithEvents scorelbl As System.Windows.Forms.Label
+    Friend WithEvents MenuPanel As System.Windows.Forms.Panel
+    Friend WithEvents Title As System.Windows.Forms.Label
+    Friend WithEvents Difficultyselector As System.Windows.Forms.ComboBox
+    Friend WithEvents Startbtn As System.Windows.Forms.Button
+    Friend WithEvents Highscoresbtn As System.Windows.Forms.Button
+    Friend WithEvents Highscorestxt As System.Windows.Forms.Label
+    Friend WithEvents ColourChanger As System.Windows.Forms.Timer
+    Friend WithEvents Fader As System.Windows.Forms.Timer
+    Friend WithEvents scoredisp As System.Windows.Forms.Label
+    Friend WithEvents SettingsBtn As System.Windows.Forms.Button
+    Private WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Private WithEvents Food As Microsoft.VisualBasic.PowerPacks.OvalShape
+    Friend WithEvents settingspanel As System.Windows.Forms.Panel
+    Friend WithEvents changecolourbtn As System.Windows.Forms.Button
+    Friend WithEvents Snakecolour As System.Windows.Forms.ColorDialog
+    Friend WithEvents savebtn As System.Windows.Forms.Button
+    Friend WithEvents TitleSnake_0 As System.Windows.Forms.Label
+    Friend WithEvents TitleMover As System.Windows.Forms.Timer
+
+End Class
---
+++ Snake/Form1.vb
@@ -0,0 +1,557 @@
+﻿'start time: 23:22
+
+'**************************
+'*SNAKE CODE BY JACK STEEL*
+'**************************
+
+Public Class ParentForm
+    Dim SnakeBody(1000) As Label
+	Dim TitleSnake(10000) As Label
+    Dim directionreq As Char
+	Dim Length_of_Snake As Integer
+	Dim Score As Integer
+	Dim showing As Boolean
+	Dim currentstate As Byte
+	Dim difficulty As Byte
+    Dim powerfood As Boolean
+	Dim FRed As Byte
+	Dim FGreen As Byte
+    Dim FBlue As Byte
+    Dim direction As Char
+    Dim colour As System.Drawing.Color
+	Dim paused As Boolean
+	Dim Titledirectionreq As Char
+	Dim TitleDirection As Char
+	Dim titlecount As Integer
+	Dim titlesnakelength As Integer
+
+
+    Private Sub InitialiseSnake()
+        Dim name As String
+
+        SnakeBody(0) = Snake_0
+        SnakeBody(1) = Snake_1
+        SnakeBody(2) = Snake_2
+        SnakeBody(3) = Snake_3
+        TitleSnake(0) = TitleSnake_0
+        'title snake
+		For i = 1 To 20
+			name = "TitleSnake_" & i
+			Dim newpartTitle As New Label
+
+			newpartTitle.Name = name
+			newpartTitle.Left = TitleSnake(i - 1).Left - 10
+			newpartTitle.Top = TitleSnake(i - 1).Top
+			newpartTitle.Visible = True
+			newpartTitle.BackColor = Color.White
+			newpartTitle.AutoSize = False
+			newpartTitle.Size = Snake_0.Size
+
+			Me.Controls.Add(newpartTitle)
+			newpartTitle.Parent = MenuPanel
+			TitleSnake(i) = newpartTitle
+		Next
+		scorelbl.SendToBack()
+		titlesnakelength = 20
+    End Sub
+
+	Private Sub TitleSnakeMover(ByVal directionnum As Integer)
+		
+		TitleSnake_0.SendToBack()
+		
+		Select Case directionnum
+			Case 1
+				If TitleDirection <> "U" Then
+					Titledirectionreq = "D"
+				End If
+
+			Case 2
+				If TitleDirection <> "L" Then
+					Titledirectionreq = "R"
+				End If
+			Case 3
+				If TitleDirection <> "D" Then
+					Titledirectionreq = "U"
+				End If
+			Case 4
+				If TitleDirection <> "R" Then
+					Titledirectionreq = "L"
+				End If
+		End Select
+
+		TitleDirection = Titledirectionreq
+		'Update snake
+		For i = titlesnakelength To 1 Step -1
+			TitleSnake(i).Top = TitleSnake(i - 1).Top
+			TitleSnake(i).Left = TitleSnake(i - 1).Left
+			TitleSnake(i).SendToBack()
+		Next
+		'end
+
+		If TitleDirection = "R" Then
+			TitleSnake(0).Left += 10
+		ElseIf TitleDirection = "L" Then
+			TitleSnake(0).Left -= 10
+		ElseIf TitleDirection = "D" Then
+			TitleSnake(0).Top += 10
+		ElseIf TitleDirection = "U" Then
+			TitleSnake(0).Top -= 10
+		End If
+
+
+
+		If TitleSnake(0).Left >= Me.Width - 100 Then
+			TitleSnake(0).Left -= 10
+			TitleDirection = "L"
+		End If
+		If TitleSnake(0).Left <= 100 Then
+			TitleSnake(0).Left += 10
+			TitleDirection = "R"
+
+		End If
+		If TitleSnake(0).Top <= 100 Then
+			TitleSnake(0).Top += 10
+			TitleDirection = "D"
+
+		End If
+		If TitleSnake(0).Top >= Me.Height - 50 Then
+			TitleSnake(0).Top -= 10
+			TitleDirection = "U"
+
+		End If
+
+
+
+		
+
+
+
+		
+
+
+	End Sub
+
+	Private Sub growtitlesnake()
+		Dim name As String
+		name = "TitleSnake_" & titlesnakelength + 1
+		Dim newpart As New Label
+
+		newpart.Name = name
+		newpart.Left = TitleSnake(titlesnakelength).Left - 10
+		newpart.Top = TitleSnake(titlesnakelength).Top
+		newpart.Visible = True
+		newpart.BackColor = Color.White
+		newpart.AutoSize = False
+		newpart.Size = TitleSnake_0.Size
+
+		Me.Controls.Add(newpart)
+		newpart.Parent = MenuPanel
+		TitleSnake(titlesnakelength + 1) = newpart
+		titlesnakelength += 1
+	End Sub
+	Private Sub ParentForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Length_of_Snake = 3
+		direction = "R"
+		InitialiseSnake()
+		MenuPanel.Size = Me.Size
+		MenuPanel.Top = 0
+        MenuPanel.Left = 0
+		colour = Color.White
+		TitleDirection = "R"
+		My.Computer.Audio.Play(My.Resources.SnakeTheme, AudioPlayMode.BackgroundLoop)
+	End Sub
+
+	Private Sub Mover_Tick(sender As Object, e As EventArgs) Handles Mover.Tick
+        UpdateSnake()
+        If directionreq = "R" Then
+            direction = "R"
+        ElseIf directionreq = "L" Then
+            direction = "L"
+        ElseIf directionreq = "D" Then
+            direction = "D"
+        ElseIf directionreq = "U" Then
+            direction = "U"
+        End If
+        If direction = "R" Then
+            SnakeBody(0).Left += 10
+        ElseIf direction = "L" Then
+            SnakeBody(0).Left -= 10
+        ElseIf direction = "D" Then
+            SnakeBody(0).Top += 10
+        ElseIf direction = "U" Then
+            SnakeBody(0).Top -= 10
+        End If
+
+		'Collisions
+		For i = 1 To Length_of_Snake
+			If SnakeBody(0).Bounds.IntersectsWith(SnakeBody(i).Bounds) Then
+				Mover.Enabled = False
+				DeathOccured()
+				Exit For
+			ElseIf SnakeBody(0).Left < 0 Or SnakeBody(0).Left > Me.Width - 10 Or SnakeBody(0).Top < 0 Or SnakeBody(0).Top > Me.Height - 10 Then
+				Mover.Enabled = False
+				DeathOccured()
+				Exit For
+			End If
+
+
+		Next
+
+	End Sub
+	Private Sub deathoccured()
+		Dim name As String
+
+		name = InputBox("Your Final score is: " & Score & Chr(13) & "Enter your name")
+		If Not Score < 0 And Not name = "" Then
+			FileOpen(1, "Highscores.txt", OpenMode.Append)
+			PrintLine(1, name & ": " & Score & Chr(13))
+			FileClose(1)
+		End If
+		MenuPanel.Show()
+
+		'Reset
+		InitialiseSnake()
+		For i = 4 To Length_of_Snake
+			Me.Controls.Remove(SnakeBody(i))
+		Next
+		Length_of_Snake = 3
+        directionreq = "R"
+		SnakeBody(0).Left = 250
+		SnakeBody(0).Top = 210
+        SnakeBody(1).Left = 240
+        SnakeBody(1).Top = 210
+        SnakeBody(2).Left = 230
+        SnakeBody(2).Top = 210
+        SnakeBody(3).Left = 220
+        SnakeBody(3).Top = 210
+		GenerateFood()
+		TitleMover.Start()
+	End Sub
+	Private Sub UpdateSnake()
+		For i = Length_of_Snake To 1 Step -1
+			SnakeBody(i).Top = SnakeBody(i - 1).Top
+			SnakeBody(i).Left = SnakeBody(i - 1).Left
+		Next
+		If SnakeBody(0).Bounds.IntersectsWith(Food.Bounds) Then
+			GrowSnake()
+            GenerateFood()
+		End If
+	End Sub
+
+	Private Sub GrowSnake()
+		Dim name As String
+		name = "Snake_" & Length_of_Snake + 1
+		Dim newpart As New Label
+
+		newpart.Name = name
+		newpart.Left = SnakeBody(Length_of_Snake).Left - 10
+		newpart.Top = SnakeBody(Length_of_Snake).Top
+		newpart.Visible = True
+		newpart.BackColor = Food.BackColor
+		newpart.AutoSize = False
+		newpart.Size = Snake_0.Size
+
+		Me.Controls.Add(newpart)
+		SnakeBody(Length_of_Snake + 1) = newpart
+
+		If powerfood = True Then
+			Score += 100 * difficulty + (Length_of_Snake \ 3)
+			scoredisp.Text = 100 * difficulty + (Length_of_Snake \ 3)
+            scoredisp.ForeColor = colour
+			scoredisp.Location = Food.Location
+			scoredisp.SendToBack()
+			scoredisp.Show()
+			FRed = 255
+			FBlue = 255
+			FGreen = 255
+			Fader.Start()
+		Else
+			Score = Score + ((Length_of_Snake \ 3) * difficulty)
+			scoredisp.Text = ((Length_of_Snake \ 3) * difficulty)
+            scoredisp.ForeColor = colour
+			scoredisp.Location = Food.Location
+			scoredisp.SendToBack()
+			scoredisp.Show()
+			FRed = 255
+			FBlue = 255
+			FGreen = 255
+			Fader.Start()
+		End If
+
+		scorelbl.Text = "Score: " & Score
+		Length_of_Snake += 1
+
+
+
+
+
+
+	End Sub
+	Private Sub DirectionChangeKey(sender As Object, e As KeyEventArgs) Handles MyBase.KeyDown
+		If e.KeyData = Keys.S Or e.KeyData = Keys.Down Then
+			If direction <> "U" Then
+                directionreq = "D"
+			End If
+
+		End If
+		If e.KeyData = Keys.D Or e.KeyData = Keys.Right Then
+			If direction <> "L" Then
+                directionreq = "R"
+			End If
+		End If
+		If e.KeyData = Keys.A Or e.KeyData = Keys.Left Then
+			If direction <> "R" Then
+                directionreq = "L"
+			End If
+
+		End If
+		If e.KeyData = Keys.W Or e.KeyData = Keys.Up Then
+			If direction <> "D" Then
+                directionreq = "U"
+			End If
+        End If
+        If e.KeyData = Keys.P Then
+            If paused = True Then
+                paused = False
+                Mover.Start()
+            Else
+                paused = True
+                Mover.Stop()
+            End If
+        End If
+
+	End Sub
+
+	Private Sub GenerateFood()
+		Dim toppos As Integer
+		Dim leftpos As Integer
+		Dim powerchance As Integer
+
+
+		Randomize()
+		toppos = ((Int(((Me.Height - 100) - 1 + 1) * Rnd() + 1)) \ 10) * 10
+		Randomize()
+		leftpos = ((Int(((Me.Width - 100) - 1 + 1) * Rnd() + 1)) \ 10) * 10
+
+		Randomize()
+		powerchance = Int((100 - 0 + 1) * Rnd() + 0)
+		If powerchance > 90 Then
+			ColourChanger.Start()
+			powerfood = True
+		Else
+			ColourChanger.Stop()
+			Food.BackColor = Color.White
+			powerfood = False
+		End If
+
+
+		Dim CRed As Integer
+		Dim CBlue As Integer
+		Dim CGreen As Integer
+
+		Randomize()
+		CRed = Int((255 - 10 + 1) * Rnd() + 10)
+		Randomize()
+		CBlue = Int((255 - 10 + 1) * Rnd() + 10)
+		Randomize()
+		CGreen = Int((255 - 10 + 1) * Rnd() + 10)
+
+
+		Food.BackColor = (Color.FromArgb(CRed, CGreen, CBlue))
+
+
+		Food.Top = toppos
+		Food.Left = leftpos
+		Food.Show()
+
+	End Sub
+
+	Private Sub resizemenu(sender As Object, e As EventArgs) Handles MyBase.SizeChanged
+		MenuPanel.Size = Me.Size
+	End Sub
+
+	Private Sub Startbtn_Click(sender As Object, e As EventArgs) Handles Startbtn.Click
+		If Difficultyselector.Text = "Select Difficulty" Then
+			MsgBox("Please Select a Difficulty", MsgBoxStyle.Exclamation, "Difficulty Error")
+
+		ElseIf Difficultyselector.Text = "Easy" Then
+			Mover.Interval = 100
+			MenuPanel.Hide()
+			Mover.Enabled = True
+			difficulty = 1
+			TitleMover.Stop()
+		ElseIf Difficultyselector.Text = "Intermediate" Then
+			Mover.Interval = 75
+			MenuPanel.Hide()
+			Mover.Enabled = True
+			difficulty = 2
+			TitleMover.Stop()
+		ElseIf Difficultyselector.Text = "Hard" Then
+			Mover.Interval = 50
+			MenuPanel.Hide()
+			Mover.Enabled = True
+			difficulty = 3
+			TitleMover.Stop()
+		ElseIf Difficultyselector.Text = "Insane" Then
+			Mover.Interval = 20
+			MenuPanel.Hide()
+			Mover.Enabled = True
+			difficulty = 4
+			TitleMover.Stop()
+		ElseIf Difficultyselector.Text = "Impossible" Then
+			Mover.Interval = 1
+			MenuPanel.Hide()
+			Mover.Enabled = True
+			difficulty = 5
+			TitleMover.Stop()
+		End If
+
+		Score = 0
+		scorelbl.Text = "Score: " & Score
+
+
+	End Sub
+
+	Private Sub Highscoresbtn_Click(sender As Object, e As EventArgs) Handles Highscoresbtn.Click
+
+		Dim currentline As String
+		Dim splitrecord() As String
+		Dim arrayelements As Integer
+		Dim holdingname As String
+		Dim holdingscore As Integer
+		Dim namearr(100) As String
+		Dim scorearr(100) As Integer
+		Dim count As Integer = 0
+
+		FileOpen(1, "Highscores.txt", OpenMode.Input)
+		Do Until EOF(1) = True
+			currentline = LineInput(1)
+			splitrecord = currentline.Split(":")
+			namearr(count) = splitrecord(0)
+			Try
+				scorearr(count) = splitrecord(1)
+			Catch ex As Exception
+				count = count - 1
+			End Try
+
+			count = count + 1
+
+		Loop
+		FileClose(1)
+		arrayelements = count - 2
+		For i = 0 To arrayelements
+
+			For j = 0 To arrayelements
+				If scorearr(j + 1) > scorearr(j) Then
+					holdingscore = scorearr(j)
+					holdingname = namearr(j)
+					scorearr(j) = scorearr(j + 1)
+					namearr(j) = namearr(j + 1)
+					scorearr(j + 1) = holdingscore
+					namearr(j + 1) = holdingname
+
+
+				End If
+			Next
+		Next
+
+		If showing = False Then
+			If Me.WindowState = FormWindowState.Maximized Then
+				currentstate = 0
+			Else
+				currentstate = 1
+			End If
+
+			Me.WindowState = FormWindowState.Maximized
+			Highscorestxt.Show()
+			Highscorestxt.Text = "HIGHSCORES: " & Chr(13) & Chr(13)
+			For i = 0 To count - 1
+				Highscorestxt.Text = Highscorestxt.Text & (namearr(i)) & ": " & scorearr(i) & Chr(13)
+			Next
+			showing = True
+		ElseIf showing = True Then
+			If currentstate = 0 Then
+				Me.WindowState = FormWindowState.Maximized
+			ElseIf currentstate = 1 Then
+				Me.WindowState = FormWindowState.Normal
+			End If
+
+			Highscorestxt.Hide()
+			showing = False
+		End If
+	End Sub
+
+	Private Sub ColourChanger_Tick(sender As Object, e As EventArgs) Handles ColourChanger.Tick
+		Dim CRed As Integer
+		Dim CBlue As Integer
+		Dim CGreen As Integer
+
+		Randomize()
+		CRed = Int((255 - 1 + 1) * Rnd() + 1)
+		Randomize()
+		CBlue = Int((255 - 1 + 1) * Rnd() + 1)
+		Randomize()
+		CGreen = Int((255 - 1 + 1) * Rnd() + 1)
+
+
+		Food.BackColor = (Color.FromArgb(CRed, CGreen, CBlue))
+
+	End Sub
+
+	Private Sub Fader_Tick(sender As Object, e As EventArgs) Handles Fader.Tick
+		If FRed >= 5 Then
+			FRed -= 5
+			FBlue -= 5
+			FGreen -= 5
+			scoredisp.Top -= 1
+		Else
+			FRed = 255
+			FBlue = 255
+			FGreen = 255
+			scoredisp.Hide()
+			Fader.Stop()
+		End If
+
+
+		scoredisp.ForeColor = Color.FromArgb(FRed, FBlue, FGreen)
+	End Sub
+
+    Private Sub SettingsBtn_Click(sender As Object, e As EventArgs) Handles SettingsBtn.Click
+        settingspanel.Show()
+
+    End Sub
+
+    Private Sub changecolourbtn_Click(sender As Object, e As EventArgs) Handles changecolourbtn.Click
+
+        If Snakecolour.ShowDialog <> Windows.Forms.DialogResult.Cancel Then
+
+            colour = Snakecolour.Color
+            Snake_0.BackColor = colour
+            Snake_1.BackColor = colour
+            Snake_2.BackColor = colour
+            Snake_3.BackColor = colour
+        End If
+    End Sub
+
+    Private Sub savebtn_Click(sender As Object, e As EventArgs) Handles savebtn.Click
+        settingspanel.Hide()
+
+    End Sub
+
+	Private Sub TitleMover_Tick(sender As Object, e As EventArgs) Handles TitleMover.Tick
+		Dim directionnumd As Integer
+
+		If titlecount >= 3 Then
+			Randomize()
+			directionnumd = Int((4 - 1 + 1) * Rnd() + 1)
+			titlecount = 0
+			growtitlesnake()
+		End If
+		
+		TitleSnakeMover(directionnumd)
+		titlecount += 1
+
+	End Sub
+End Class
+
+'End time: 00:09
